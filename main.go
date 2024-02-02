796a83349d55eac5779e71ddc17095cc945d79fa
daquentin <quentin.dassivignon@ynov.com>
2024-02-02 15:02:37 +0100

    Collect All Data

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "Groupie-Trackers/functions"
+import (
+	"Groupie-Trackers/functions"
+	"fmt"
+)
 
 func main() {
-	functions.GetApiUrl()
+	API := functions.ApiUrl()
+	fmt.Println(API)
+
+	ArtistData := functions.ArtistData()
+	fmt.Println(ArtistData[0])
+
+	LocationsData := functions.LocationsData()
+	fmt.Println(LocationsData.Index[0])
+
+	DatesData := functions.DatesData()
+	fmt.Println(DatesData.Index[0])
+
+	RelationsData := functions.RelationData()
+	fmt.Println(RelationsData.Index[0])
 }
