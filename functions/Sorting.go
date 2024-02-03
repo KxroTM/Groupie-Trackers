31dc00d1fbc6e2fa836e61e7ec7e7ecc723c33a6
daquentin <quentin.dassivignon@ynov.com>
2024-02-03 13:51:43 +0100

    Artist by Album Date and Number of Member

--- functions/Sorting.go
+++ functions/Sorting.go
@@ -1,10 +1,30 @@
 package functions
 
-func ArtistListbyCreationDate(allArtists AllArtistsData, date int) AllArtistsData {
+func ArtistbyCreationDate(allArtists AllArtistsData, date int) AllArtistsData {
 	var artistList []Artist
 
 	for i := 0; i < len(allArtists); i++ {
 		if allArtists[i].CreationDate == int64(date) {
+			artistList = append(artistList, allArtists[i])
+		}
+	}
+	return artistList
+}
+
+func ArtistbyFirstAlbumDate(allArtists AllArtistsData, date string) AllArtistsData {
+	var artistList []Artist
+	for i := 0; i < len(allArtists); i++ {
+		if allArtists[i].FirstAlbum == date {
+			artistList = append(artistList, allArtists[i])
+		}
+	}
+	return artistList
+}
+
+func ArtistbyNumberofMember(allArtists AllArtistsData, nomber int) AllArtistsData {
+	var artistList []Artist
+	for i := 0; i < len(allArtists); i++ {
+		if len(allArtists[i].Members) == nomber {
 			artistList = append(artistList, allArtists[i])
 		}
 	}
--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 )
 
 func main() {
-	API := functions.ApiUrl()
-	fmt.Println(API)
+	// API := functions.ApiUrl()
+	// fmt.Println(API)
 
 	ArtistData := functions.ArtistData()
 	fmt.Println(ArtistData[0])
 
-	LocationsData := functions.LocationsData()
-	fmt.Println(LocationsData.Index[0])
+	// LocationsData := functions.LocationsData()
+	// fmt.Println(LocationsData.Index[0])
 
-	DatesData := functions.DatesData()
-	fmt.Println(DatesData.Index[0])
+	// DatesData := functions.DatesData()
+	// fmt.Println(DatesData.Index[0])
 
-	RelationsData := functions.RelationData()
-	fmt.Println(RelationsData.Index[0])
+	// RelationsData := functions.RelationData()
+	// fmt.Println(RelationsData.Index[0])
+
 }
