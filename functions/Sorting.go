737d831bb9cc84d3c616d40acbb0a03cd9603056
daquentin <quentin.dassivignon@ynov.com>
2024-02-03 14:03:42 +0100

    Artist by Date

--- functions/Sorting.go
+++ functions/Sorting.go
@@ -1,30 +1,10 @@
 package functions
 
-func ArtistbyCreationDate(allArtists AllArtistsData, date int) AllArtistsData {
-	var artistList []Artist
+func ArtistbyCreationDate(allArtists AllArtistsData, startdate, enddate int) AllArtistsData {
+	var artistList AllArtistsData
 
 	for i := 0; i < len(allArtists); i++ {
-		if allArtists[i].CreationDate == int64(date) {
-			artistList = append(artistList, allArtists[i])
-		}
-	}
-	return artistList
-}
-
-func ArtistbyFirstAlbumDate(allArtists AllArtistsData, date string) AllArtistsData {
-	var artistList []Artist
-	for i := 0; i < len(allArtists); i++ {
-		if allArtists[i].FirstAlbum == date {
-			artistList = append(artistList, allArtists[i])
-		}
-	}
-	return artistList
-}
-
-func ArtistbyNumberofMember(allArtists AllArtistsData, nomber int) AllArtistsData {
-	var artistList []Artist
-	for i := 0; i < len(allArtists); i++ {
-		if len(allArtists[i].Members) == nomber {
+		if int64(startdate) <= allArtists[i].CreationDate && allArtists[i].CreationDate <= int64(enddate) {
 			artistList = append(artistList, allArtists[i])
 		}
 	}
