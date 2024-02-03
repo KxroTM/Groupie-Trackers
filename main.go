6e37ebc83dc6b0a916226724faa57be2d912c989
ayoussef <youssef.ammari@ynov.com>
2024-02-03 21:00:40 +0100

    added reseach system

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-// import Groupie_Trackers "Groupie_Trackers/tools"
+import (
+	Groupie_Trackers "Groupie_Trackers/tools"
+)
 
 func main() {
 	// //Appeler la fonction login sur la page de connection
 	// Groupie_Trackers.Login("chems", "mdp")
 	// //Appeler la fonction register si la fonction login returne false
 	// Groupie_Trackers.Register("test", "mdp", "mauvaismdp")
+
+	Groupie_Trackers.FinalResearch("acdc")
+	// for _, data := range datas {
+	// 	fmt.Println(data.Name)
+	// }
 }
--- tools/search.go
+++ tools/search.go
@@ -1,1 +1,42 @@
 package Groupie_Trackers
+
+import (
+	"fmt"
+	"strings"
+)
+
+func SearchByMember(input string) []Artist {
+	data := ArtistData()
+	var result []Artist
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data[i].Members); j++ {
+			if strings.Contains(strings.ToLower(data[i].Members[j]), strings.ToLower(input)) {
+				result = append(result, data[i])
+			}
+		}
+	}
+	return result
+}
+
+func SearchByName(input string) Artist {
+	data := ArtistData()
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data[i].Name); j++ {
+			if strings.Contains(strings.ToLower(data[i].Name), strings.ToLower(input)) {
+				return data[i]
+			}
+		}
+	}
+	return Artist{}
+}
+
+func FinalResearch(input string) {
+	result := SearchByMember(input)
+	result2 := SearchByName(input)
+
+	if len(result) != 0 {
+		fmt.Println(result)
+	} else if result2.Name != "" {
+		fmt.Println(result2)
+	}
+}
