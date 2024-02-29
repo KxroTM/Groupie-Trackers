aa1f77f951ce8b7ab23db229d82cfefd62da9a6e
Yohann Prieux <tanitay93140@gmail.com>
2024-02-29 10:38:35 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	app.Homepage() // Appel de la fonction Homepage du fichier homepage.go
+	app.Mainpage() // Appel de la fonction Mainpage du fichier mainpage.go
 }
 
 func init() {
